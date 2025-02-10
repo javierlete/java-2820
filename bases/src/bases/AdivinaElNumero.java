435319fa80f0b631fe46dd59ba6bc274e1d06b95
javier <javier@ipartekaula.com>
2025-02-10 17:08:38 +0100

    Mejores puntuaciones

--- bases/src/bases/AdivinaElNumero.java
+++ bases/src/bases/AdivinaElNumero.java
@@ -3,8 +3,6 @@ package bases;
 import java.util.Random;
 import java.util.Scanner;
 
-// Puntuación mínima
-
 public class AdivinaElNumero {
 
 	private static final int TOPE_INTENTOS = 5;
@@ -18,6 +16,8 @@ public class AdivinaElNumero {
 		System.out.print("Máximo: ");
 		int maximo = sc.nextInt();
 
+		Integer mejorPuntuacion = null;
+		
 		String otraPartida;
 		
 		do {
@@ -54,6 +54,14 @@ public class AdivinaElNumero {
 				} else if (numero == suNumero) {
 					System.out.println("Has acertado");
 					System.out.println("Te ha costado " + intentos + " intentos");
+					
+					if(mejorPuntuacion == null || intentos < mejorPuntuacion) {
+						System.out.println("Felicidades. Has conseguido la mejor puntuación");
+						mejorPuntuacion = intentos;
+					}
+					
+					System.out.println("La mejor puntuación es " + mejorPuntuacion);
+					
 					repetir = false;
 				} else if (seHaPasado) {
 					System.out.println("Te has pasado de intentos");
