7c4f5bf8e449b30f9bacfb8dcc4b8d043216b717
javier <javier@ipartekaula.com>
2025-02-10 16:20:33 +0100

    Con tope de intentos

--- bases/src/bases/AdivinaElNumero.java
+++ bases/src/bases/AdivinaElNumero.java
@@ -3,46 +3,56 @@ package bases;
 import java.util.Random;
 import java.util.Scanner;
 
-// Tope de intentos
 // Detección de límites
 // Selección de límites
 // Preguntar si se quiere otra partida
 // Puntuación mínima
 
 public class AdivinaElNumero {
+	private static final int TOPE_INTENTOS = 10;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		// Buscar un número aleatorio
 		int numero = new Random().nextInt(0, 101);
 
-//		System.out.println(numero);
+		System.out.println(numero);
 
 		int intentos = 0;
-
+		
 		int suNumero;
+		
+		boolean repetir = true;
 
+		boolean seHaPasado = false;
+		
 		do {
 			// Pedir un número
 			System.out.print("Dime un número: ");
 			suNumero = sc.nextInt();
 			intentos++;
-			
+
 //			System.out.println(suNumero);
+
+			seHaPasado = intentos >= TOPE_INTENTOS;
 			
 			// Comprobar si el número es mayor que el introducido
-			if (numero > suNumero) {
+			if (numero > suNumero && !seHaPasado) {
 				System.out.println("El número es mayor");
-			} else if (numero < suNumero) {
+			} else if (numero < suNumero && !seHaPasado) {
 				System.out.println("El número es menor");
-			} else {
+			} else if (numero == suNumero) {
 				System.out.println("Has acertado");
 				System.out.println("Te ha costado " + intentos + " intentos");
+				repetir = false;
+			} else if (seHaPasado) {
+				System.out.println("Te has pasado de intentos");
+				repetir = false;
 			}
-			
-		} while (numero != suNumero);
+		} while (repetir);
 		// Repetir mientras no se haya acertado
-		
+
 		sc.close();
 	}
 }
