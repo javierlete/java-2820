34f475f95c65d4b225dccf7f7e3377306883acc9
javier <javier@ipartekaula.com>
2025-02-10 16:56:44 +0100

    El usuario puede echar otras partidas

--- bases/src/bases/AdivinaElNumero.java
+++ bases/src/bases/AdivinaElNumero.java
@@ -3,7 +3,6 @@ package bases;
 import java.util.Random;
 import java.util.Scanner;
 
-// Preguntar si se quiere otra partida
 // Puntuación mínima
 
 public class AdivinaElNumero {
@@ -19,52 +18,55 @@ public class AdivinaElNumero {
 		System.out.print("Máximo: ");
 		int maximo = sc.nextInt();
 
-		// Buscar un número aleatorio
-		int numero = new Random().nextInt(minimo, maximo + 1);
-
-		System.out.println(numero);
-
-		int intentos = 0;
-		
-		int suNumero;
-		
-		boolean repetir = true;
-
-		boolean seHaPasado = false;
+		String otraPartida;
 		
 		do {
+			// Buscar un número aleatorio
+			int numero = new Random().nextInt(minimo, maximo + 1);
+			System.out.println(numero);
+			int intentos = 0;
+			int suNumero;
+			boolean repetir = true;
+			boolean seHaPasado = false;
 			do {
-				// Pedir un número
-				System.out.print("Dime un número: ");
-				suNumero = sc.nextInt();
-				
-				if(suNumero < minimo  || suNumero > maximo) {
-					System.out.println("El número debe ser entre " + minimo + " y " + maximo);
+				do {
+					// Pedir un número
+					System.out.print("Dime un número: ");
+					suNumero = sc.nextInt();
+					sc.nextLine();
+
+					if (suNumero < minimo || suNumero > maximo) {
+						System.out.println("El número debe ser entre " + minimo + " y " + maximo);
+					}
+				} while (suNumero < minimo || suNumero > maximo);
+
+				intentos++;
+
+				//			System.out.println(suNumero);
+
+				seHaPasado = intentos >= TOPE_INTENTOS;
+
+				// Comprobar si el número es mayor que el introducido
+				if (numero > suNumero && !seHaPasado) {
+					System.out.println("El número es mayor");
+				} else if (numero < suNumero && !seHaPasado) {
+					System.out.println("El número es menor");
+				} else if (numero == suNumero) {
+					System.out.println("Has acertado");
+					System.out.println("Te ha costado " + intentos + " intentos");
+					repetir = false;
+				} else if (seHaPasado) {
+					System.out.println("Te has pasado de intentos");
+					repetir = false;
 				}
-			} while (suNumero < minimo  || suNumero > maximo);
+			} while (repetir);
+			// Repetir mientras no se haya acertado
 			
-			intentos++;
-
-//			System.out.println(suNumero);
-
-			seHaPasado = intentos >= TOPE_INTENTOS;
+			System.out.print("¿Otra partida? (s/N) ");
+			otraPartida = sc.nextLine();
 			
-			// Comprobar si el número es mayor que el introducido
-			if (numero > suNumero && !seHaPasado) {
-				System.out.println("El número es mayor");
-			} else if (numero < suNumero && !seHaPasado) {
-				System.out.println("El número es menor");
-			} else if (numero == suNumero) {
-				System.out.println("Has acertado");
-				System.out.println("Te ha costado " + intentos + " intentos");
-				repetir = false;
-			} else if (seHaPasado) {
-				System.out.println("Te has pasado de intentos");
-				repetir = false;
-			}
-		} while (repetir);
-		// Repetir mientras no se haya acertado
-
+		} while (otraPartida.equalsIgnoreCase("s"));
+		
 		sc.close();
 	}
 }
