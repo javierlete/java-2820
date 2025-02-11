96e15b1297408e7ea90929f1f8d47518aa25d559
javier <javier@ipartekaula.com>
2025-02-11 18:17:06 +0100

    Ejercicios propuestos

---
+++ bases/src/ejercicios/Ahorcado.java
@@ -0,0 +1,21 @@
+package ejercicios;
+
+/*
+ * Juego en el que hay que adivinar por letras una palabra
+ * 
+ * El juego tendrá una lista de palabras almacenada en una variable
+ * 
+ * El programa seleccionará una palabra al azar
+ * 
+ * El usuario debe de proporcionar letras por cada turno
+ * 
+ * Si el usuario acierta con una letra existente, no cuenta intento
+ * 
+ * Si el usuario falla, se cuenta un intento
+ * 
+ * Al de un cierto número tope de intentos el usuario pierde
+ */
+
+public class Ahorcado {
+
+}
---
+++ bases/src/ejercicios/LocalOcupacion.java
@@ -0,0 +1,21 @@
+package ejercicios;
+
+/*
+ * El programa almacenará un listado de personas en un ArrayList
+ * 
+ * El menú es el que está definido abajo
+ */
+
+public class LocalOcupacion {
+	public static void main(String[] args) {
+		System.out.println("""
+				1. Listado
+				2. Mostrar número de personas
+				3. Añadir
+				4. Modificar
+				5. Quitar por nombre
+				
+				0. Salir
+				""");
+	}
+}
