950c35da4e3ad22eb9a8bb7cc722ee04e62e1392
javier <javier@ipartekaula.com>
2025-02-11 17:34:36 +0100

    Arrays

---
+++ bases/src/bases/EjemploArrays.java
@@ -0,0 +1,26 @@
+package bases;
+
+import java.util.Arrays;
+
+public class EjemploArrays {
+	public static void main(String[] args) {
+		int tamano = 3;
+
+		String[] textos = new String[tamano];
+
+		textos[0] = "Uno";
+		textos[1] = "Dos";
+		textos[2] = "Tres";
+
+		System.out.println(textos);
+		System.out.println(Arrays.toString(textos));
+
+		for (int i = 0; i < textos.length; i++) {
+			System.out.println(textos[i]);
+		}
+
+		for (String dato : textos) {
+			System.out.println(dato);
+		}
+	}
+}
