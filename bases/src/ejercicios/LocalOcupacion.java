b35822c60ff02e25ef46dde8d187fbbd7a79cf27
javier <javier@ipartekaula.com>
2025-02-11 18:17:06 +0100

    Ejercicios propuestos

---
+++ bases/src/ejercicios/LocalOcupacion.java
@@ -0,0 +1,19 @@
+package ejercicios;
+
+/*
+ * El programa almacenará un listado de personas en un ArrayList
+ */
+
+public class LocalOcupacion {
+	public static void main(String[] args) {
+		System.out.println("""
+				1. Listado
+				2. Mostrar número de personas
+				3. Añadir
+				4. Modificar
+				5. Quitar por nombre
+				
+				0. Salir
+				""");
+	}
+}
