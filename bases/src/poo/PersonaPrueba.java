f0f960a47fa5b3121873c1131eed3195461a62b9
javier <javier@ipartekaula.com>
2025-02-11 17:34:46 +0100

    Ejemplo de Persona con arrays

--- bases/src/poo/PersonaPrueba.java
+++ bases/src/poo/PersonaPrueba.java
@@ -19,5 +19,15 @@ public class PersonaPrueba {
 		Persona p2 = new Persona("     María    ", "Femenino", 34);
 		
 		System.out.println(p2);
+		
+		Persona[] personas = new Persona[3];
+		
+		personas[0] = p;
+		personas[1] = p2;
+		personas[2] = new Persona(" nueva ", "Femenino", 56);
+		
+		for(Persona per: personas) {
+			System.out.println(per);
+		}
 	}
 }
