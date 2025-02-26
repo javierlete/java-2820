754d512dee26dc7a4e94f4f557fab47b3bc0ccd1
javier <javier@ipartekaula.com>
2025-02-26 17:10:13 +0100

    Controlador de reservas mostrando en crudo el listado

---
+++ tpv/src/main/java/controladores/ReservasServlet.java
@@ -0,0 +1,22 @@
+package controladores;
+
+import java.io.IOException;
+
+import daos.ReservaDao;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/reservas")
+public class ReservasServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ReservaDao dao = new ReservaDao("jdbc:sqlite:C:\\Users\\java.IPARTEKAULA\\git\\java-2820\\tpv\\bdd\\tpv.sqlite", "", "");
+		
+		response.getWriter().println(dao.buscarTodos());
+	}
+
+}
