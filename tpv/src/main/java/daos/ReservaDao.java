f41225c85a052b617203073a40640324b834484c
javier <javier@ipartekaula.com>
2025-02-26 17:03:49 +0100

    ReservaDao

--- tpv/src/main/java/daos/ReservaDao.java
+++ tpv/src/main/java/daos/ReservaDao.java
@@ -2,67 +2,83 @@ package daos;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 import bibliotecas.BaseDeDatos;
-import modelos.Producto;
+import modelos.Reserva;
 
-// DAO: Data Access Object
-
-public class ProductoDao {
-	private static final String SQL_SELECT = "SELECT * FROM productos";
+public class ReservaDao {
+	private static final String SQL_SELECT = "SELECT * FROM reservas";
 	private static final String SQL_SELECT_ID = SQL_SELECT + " WHERE id=";
 
-	private static final String SQL_INSERT = "INSERT INTO productos (nombre, precio, descripcion) VALUES ('%s', %s, '%s')";
-	private static final String SQL_UPDATE = "UPDATE productos SET nombre='%s', precio=%s, descripcion='%s' WHERE id=%s";
-	private static final String SQL_DELETE = "DELETE FROM productos WHERE id=";
+	private static final String SQL_INSERT = "INSERT INTO reservas (nombre, cuantos, hora) VALUES ('%s', %s, '%s')";
+	private static final String SQL_UPDATE = "UPDATE reservas SET nombre='%s', cuantos=%s, hora='%s' WHERE id=%s";
+	private static final String SQL_DELETE = "DELETE FROM reservas WHERE id=";
 
 	private BaseDeDatos bdd;
 
-	public ProductoDao(String url, String usuario, String password) {
+	public ReservaDao(String url, String usuario, String password) {
 		bdd = new BaseDeDatos(url, usuario, password);
 	}
 
-	public ArrayList<Producto> buscarTodos() {
+	public ArrayList<Reserva> buscarTodos() {
 		try (ResultSet rs = bdd.consulta(SQL_SELECT)) {
-			ArrayList<Producto> productos = new ArrayList<>();
+			ArrayList<Reserva> reservas = new ArrayList<>();
 
 			while (rs.next()) {
-				Producto producto = new Producto(rs.getInt("id"), rs.getString("nombre"), rs.getDouble("precio"),
-						rs.getString("descripcion"));
-				productos.add(producto);
+				Integer id = rs.getInt("id");
+				String nombre = rs.getString("nombre");
+				int cuantos = rs.getInt("cuantos");
+				LocalDateTime hora = LocalDateTime.parse(rs.getString("hora"));
+
+				Reserva reserva = new Reserva(id, nombre, cuantos, hora);
+
+				reservas.add(reserva);
 			}
 
-			return productos;
+			return reservas;
 		} catch (SQLException e) {
 			throw new RuntimeException("Ha habido un error en la consulta a la base de datos", e);
 		}
 	}
 
-	public Producto buscarPorId(int id) {
+	public Reserva buscarPorId(int id) {
 		try (ResultSet rs = bdd.consulta(SQL_SELECT_ID + id)) {
 
-			Producto producto = null;
+			Reserva reserva = null;
 
 			if (rs.next()) {
-				producto = new Producto(rs.getInt("id"), rs.getString("nombre"), rs.getDouble("precio"),
-						rs.getString("descripcion"));
+				String nombre = rs.getString("nombre");
+				int cuantos = rs.getInt("cuantos");
+				LocalDateTime hora = LocalDateTime.parse(rs.getString("hora"));
+
+				reserva = new Reserva(id, nombre, cuantos, hora);
 			}
 
-			return producto;
+			return reserva;
 		} catch (SQLException e) {
 			throw new RuntimeException("Ha habido un error en la consulta a la base de datos", e);
 		}
 	}
 
-	public void insertar(Producto producto) {
-		String sql = String.format(SQL_INSERT, producto.getNombre(), producto.getPrecio(), producto.getDescripcion());
+	public void insertar(Reserva reserva) {
+		String nombre = reserva.getNombreCliente();
+		int cuantos = reserva.getParaCuantos();
+		String hora = reserva.getHora().toString();
+		
+		String sql = String.format(SQL_INSERT, nombre, cuantos, hora);
 
 		bdd.cambio(sql);
 	}
 
-	public void modificar(Producto producto) {
-		String sql = String.format(SQL_UPDATE, producto.getNombre(), producto.getPrecio(), producto.getDescripcion(), producto.getId());
+	public void modificar(Reserva reserva) {
+		Integer id = reserva.getId();
+		String nombre = reserva.getNombreCliente();
+		int cuantos = reserva.getParaCuantos();
+		String hora = reserva.getHora().toString();
+		
+		String sql = String.format(SQL_UPDATE, nombre, cuantos, hora, id);
 
 		bdd.cambio(sql);
 	}
