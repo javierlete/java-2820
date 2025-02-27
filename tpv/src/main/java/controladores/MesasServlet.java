f1490f85ee6840df25d1694819d27c85c38a3458
javier <javier@ipartekaula.com>
2025-02-27 16:20:07 +0100

    mesas.jsp a vistas y llamamos desde controlador con ruta mesa

---
+++ tpv/src/main/java/controladores/MesasServlet.java
@@ -0,0 +1,17 @@
+package controladores;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/mesas")
+public class MesasServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/vistas/mesas.jsp").forward(request, response);
+	}
+}
