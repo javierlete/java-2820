1a05ffde07eb3f618a99e019725b6fb5da2afd45
javier <javier@ipartekaula.com>
2025-03-03 16:38:32 +0100

    Gestión de login

--- tpv/src/main/java/controladores/LoginServlet.java
+++ tpv/src/main/java/controladores/LoginServlet.java
@@ -1,11 +1,13 @@
 package controladores;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
@@ -16,7 +18,29 @@ public class LoginServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+//		Recibir la información de la petición
+		String usuario = request.getParameter("usuario");
+		String password = request.getParameter("password");
+
+//		Convertir los datos si es necesario
+//		Empaquetar los datos en un objeto del modelo
+//		Ejecutar la lógica de negocio
+		if(usuario.equals("javier") && password.equals("ipartek")) {
+//			Almacenar objeto de modelo para la vista
+//			Saltar a la siguiente vista
+			
+			HttpSession session = request.getSession();
+			
+			session.setAttribute("usuario", usuario);
+			
+			response.sendRedirect("reservas");
+		} else {
+//			Almacenar objeto de modelo para la vista
+//			Saltar a la siguiente vista
+			response.sendRedirect("login");
+		}
+		
+
 	}
 
 }
